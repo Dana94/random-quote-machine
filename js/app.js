b4180fbe36d8a6e5741704fc58e7ebf3264104cd
Dana Ottaviani <dana.ottaviani@gmail.com>
2017-01-26 17:55:56 -0500

    Able to tweet the quotes now

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let newQuote = document.getElementById('new-quote');
 let quotePlace = document.getElementById('quote-placeholder');
-let twitterButton =document.getElementById('tweet');
+let twitterButton =document.getElementsByClassName('twitter-share-button')[0];
 $('#tweet').hide();
 
 let quotes = [{
@@ -30,25 +30,9 @@ let quotes = [{
 				'shown': false
 			  },
 			  {
-			  	'quote': '\"I am not an angel,\" I asserted; \"and I will not be one till I ' + 
-			  			'die: I will be myself. Mr. Rochester, you must neither expect nor ' + 
-			  			'exact anything celestial of me - for you will not get it, any more ' + 
-			  			'than I shall get it of you: which I do not at all anticipate.\"',
-			  	'author': 'Charlotte Bront&#235',
-			  	'description': 'Author of Jane Eyre',
-			  	'shown': false
-			  },
-			  {
 			  	'quote': '\"Earth provides enough to satisfy every man\'s needs, but not every ' + 
 			  			'man\'s greed.\"',
 			  	'author': 'Mahatma Gandhi',
-			  	'description': '',
-			  	'shown': false
-			  },
-			  {
-			  	'quote': '\"Lighthouses don\'t go running all over an island looking for boats to ' + 
-			  			'save\; they just stand there shining.\"',
-			  	'author': 'Anne Lamott, author',
 			  	'description': '',
 			  	'shown': false
 			  },
@@ -62,7 +46,7 @@ let quotes = [{
 			  {
 			  	'quote': '\"I dream of giving birth to a child who will ask, "Mother, what was war?\"',
 			  	'author': 'Eve Merriam',
-			  	'description': 'Poet and writer ',
+			  	'description': 'Poet and writer',
 			  	'shown': false
 			  },
 			  {
@@ -83,21 +67,20 @@ let quotes = [{
 			  	'author': 'Neil deGrasse Tyson',
 			  	'description': '',
 			  	'shown': false
-
 			  }
-
-
 			 ];
 
 
 let progress = 0;
 let increment = Math.ceil(100/quotes.length);
+let quote;
+let author;
 
 function reset(){
 	for(let i = 0; i < quotes.length; i++){
 		quotes[i].shown = false;
 	}
-	console.log(quotes[0]);
+
 	$('#button-place').html('<button class="btn btn-danger" id="new-quote">New Quote</button>');
     newQuote = document.getElementById('new-quote');
     newQuote.addEventListener('click', displayQuotes);
@@ -113,7 +96,9 @@ function displayQuotes(){
 			index = Math.floor(Math.random() * quotes.length);
 		}
 
-		let text =  quotes[index].quote + '<br> -' + quotes[index].author;
+		quote = quotes[index].quote;
+		author = quotes[index].author;
+		let text =  quote + '<br> -' + author;
 		quotes[index].shown = true;
 		$('#quote-placeholder').html(text);
 		progress += increment;
@@ -125,4 +110,10 @@ function displayQuotes(){
 		}
 }
 
+function tweetQuote(){
+	twitterButton.href += encodeURI(quote) + ' -' + encodeURI(author);
+}
+
 newQuote.addEventListener('click', displayQuotes);
+
+twitterButton.addEventListener('click', tweetQuote);
