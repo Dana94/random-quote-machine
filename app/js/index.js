7e853fcf2de1dc16e7f2a72d9551cb5040c88ff2
Dana <dana.ottaviani@gmail.com>
2017-09-09 09:02:25 -0400

    trying ot get over the 'require is not defined' error in index.js

--- app/js/index.js
+++ app/js/index.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 var parser = csv.parse({delimiter: ','}, function(err, data){
   	for(let i = 0; i < data.length; i++){
   		row = data[i];
+  		console.log(row);
   		
   	}
 
 
 });
 
-  //csv to read through the parser
-  fs.createReadStream('../quotes.csv').pipe(parser);
+//csv to read through the parser
+fs.createReadStream('../quotes.csv').pipe(parser);
 
