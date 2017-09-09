12341940b8126a885527256693c2f91bdb61c07a
Dana <dana.ottaviani@gmail.com>
2017-09-09 08:50:46 -0400

    adding csv package

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
-let reader = new FileReader();
+const csv = require('csv');
+const fs = require('fs');
+
+var parser = csv.parse({delimiter: ','}, function(err, data){
+  	for(let i = 0; i < data.length; i++){
+  		row = data[i];
+  		
+  	}
+
+
+});
+
+  //csv to read through the parser
+  fs.createReadStream('../quotes.csv').pipe(parser);
 
